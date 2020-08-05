859270bd6f659327ebb3f798ceb75045d04f4b4b
dai <kikuchi.dai@gmail.com>
2020-08-05 12:16:36 +0900

    Re-factores tests and materials

--- app/clothes/views.py
+++ app/clothes/views.py
@@ -7,38 +7,29 @@ from core.models import Tag, Material
 from clothes import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseClothesAttrViewSet(viewsets.GenericViewSet,
+                             mixins.ListModelMixin,
+                             mixins.CreateModelMixin):
+    """Base viewset for user owned clothes attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
-    # Override get_queryset function
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new Tag"""
+        """Create a new Object"""
         serializer.save(user=self.request.user)
 
 
-class MaterialViewSet(viewsets.GenericViewSet,
-                      mixins.ListModelMixin,
-                      mixins.CreateModelMixin):
+class TagViewSet(BaseClothesAttrViewSet):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class MaterialViewSet(BaseClothesAttrViewSet):
     """Manage ingredients in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Material.objects.all()
     serializer_class = serializers.MaterialSerializer
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """"Create a new material"""
-        serializer.save(user=self.request.user)
